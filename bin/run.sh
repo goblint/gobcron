a5c769c2f45cca2038566d46a85bdcaee8c18c47
Dr. Michael Petter <petter@in.tum.de>
2024-06-26 16:20:31 +0200

    support options for benchexec

--- bin/run.sh
+++ bin/run.sh
@@ -127,7 +127,7 @@ function main () {
 
     # relocate goblint-nightly.template.xml to the correct folder on this server
     rm -f "$basedir/nightly.xml"
-    cat "$basedir/conf/nightly-template.xml" | sed "s#SVBENCHMARKPREFIX#$(conf "instance.svbenchdir")#" > "$basedir/nightly.xml"
+    cat "$basedir/conf/nightly-template.xml" | sed "s#SVBENCHMARKPREFIX#$(conf "instance.svbenchdir")#" | sed "s#SVBENCHMARKOPTIONS#$(conf "instance.options")#" > "$basedir/nightly.xml"
     cp  "$basedir/$(conf "instance.analyzerdir")/$(conf "instance.benchconf")" "$basedir/$(conf "instance.analyzerdir")/conf.json"
 
     # perform the actual benchmark
