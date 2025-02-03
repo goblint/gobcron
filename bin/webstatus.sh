5836d86c9b9b859be3a8d1a31916e8432446fbd8
Dr. Michael Petter <petter@in.tum.de>
2025-02-03 14:34:00 +0100

    including user

--- bin/webstatus.sh
+++ bin/webstatus.sh
@@ -6,8 +6,14 @@ source "$SCRIPTDIR/../lib/library.sh"
 
 
 function f {
+    # ExposeAuthInfo yes in /etc/ssh/sshd_config
+
     echo -n "<h1>Goblint/SV-Comp Running:</h1>"
     echo "<p>Folder: $(conf "instance.basedir")</p>"
+    if [ -f "$SSH_USER_AUTH" ]; then
+        local username; username = $(grep "$(cat $SSH_USER_AUTH | awk '{ print $3} ')" .ssh/authorized_keys | awk '{ print $3} ')
+        echo "<p>User: $username</p>"
+    fi
     echo "<p>Tag: $(conf "instance.tag")</p>"
     echo "<p>Goblintversion: $(conf "instance.gitrepo")[$(conf "instance.branch")]@$(conf "instance.commit")</p>"
     ./bin/progress.sh
